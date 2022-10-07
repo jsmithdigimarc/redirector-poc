f3e541f19ae588249ee3afcb18b2e615e0faea27
Jake Smith <jsmith@digimarc.com>
2022-10-07 16:37:45 -0600

    Stubbed out contents of getRedirect (psuedo code)

--- services/redirections/src/services/redirections-service.ts
+++ services/redirections/src/services/redirections-service.ts
@@ -1,14 +1,37 @@
+import {EvrythngClient} from "../clients/evrythng-client";
+import {ActionsClient} from "../clients/actions-client";
+import {RulesEngineClient} from "../clients/rules-engine-client";
+
 export interface RedirectionsService {
-  getRedirect(shortcode: string): Promise<string>
+    getRedirect(shortcode: string): Promise<string>
 }
 
-export function RedirectionsService(): RedirectionsService {
+export function RedirectionsService(
+    evrythngClient: EvrythngClient,
+    actionsClient: ActionsClient,
+    rulesEngineClient: RulesEngineClient): RedirectionsService {
 
-  async function getRedirect(shortcode: string): Promise<string> {
-    return "";
-  }
+    async function getRedirect(shortcode: string): Promise<string> {
+        const redirect = await evrythngClient.getRedirection(shortcode);
+        if (!redirect) {
+            return ""
+        }
 
-  return {
-    getRedirect,
-  };
+        const meta = await evrythngClient.getRedirectMeta(redirect.accountId, redirect.thngId || redirect.productId)
+        if (!meta) {
+            return ""
+        }
+        const action = await actionsClient.create({});
+
+        const rules = await rulesEngineClient.evaluate(meta.rules, {
+            ...meta,
+            action
+        })
+
+        return rules.sort((a, b) => a.weight - b.weight)[0].redirectUrl;
+    }
+
+    return {
+        getRedirect,
+    };
 }
