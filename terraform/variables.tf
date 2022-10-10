91ea53c49bddbfe66d633a095d823187eb8dc591
Jake Smith <jsmith@digimarc.com>
2022-10-10 17:54:03 -0600

    Removed default version values

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,17 +8,14 @@ variable "region" {
 
 variable "actions_service_version" {
   type    = string
-  default = "latest"
 }
 
 variable "redirections_service_version" {
   type    = string
-  default = "latest"
 }
 
 variable "rules_engine_service_version" {
   type    = string
-  default = "latest"
 }
 
 variable "artifact_repository_name" {
