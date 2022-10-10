eee7b932649b87efdab56b30270213c21157be1b
Jake Smith <jsmith@digimarc.com>
2022-10-10 13:26:24 -0600

    format updates

--- services/redirections/src/clients/actions-client.ts
+++ services/redirections/src/clients/actions-client.ts
@@ -1,15 +1,15 @@
-import type {Action} from "../types";
+import type { Action } from "../types";
 
 export interface ActionsClient {
-    create(action: Action): Promise<Action>
+  create(action: Action): Promise<Action>;
 }
 
-export function ActionServiceClient(base: string): ActionsClient {
-    async function create(action: Action): Promise<Action> {
-        return {}
-    }
+export function ActionsClient(base: string): ActionsClient {
+  async function create(action: Action): Promise<Action> {
+    return {};
+  }
 
-    return {
-        create
-    }
+  return {
+    create,
+  };
 }
--- services/redirections/src/clients/rules-engine-client.ts
+++ services/redirections/src/clients/rules-engine-client.ts
@@ -1,15 +1,15 @@
-import type {Action, Rule} from "../types";
+import type { Action, Rule } from "../types";
 
 export interface RulesEngineClient {
-    evaluate(rules: Rule[], payload: any): Promise<Rule[]>
+  evaluate(rules: Rule[], payload: any): Promise<Rule[]>;
 }
 
-export function RulesEngineServiceClient(base: string): RulesEngineClient {
-    async function evaluate(rules: Rule[], payload: any): Promise<Rule[]> {
-        return []
-    }
+export function RulesEngineClient(base: string): RulesEngineClient {
+  async function evaluate(rules: Rule[], payload: any): Promise<Rule[]> {
+    return rules;
+  }
 
-    return {
-        evaluate
-    }
+  return {
+    evaluate,
+  };
 }
