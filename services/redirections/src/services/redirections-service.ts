01d85e8b1242905c5c43cb44ef00a679c775993a
Jake Smith <jsmith@digimarc.com>
2022-10-10 13:26:00 -0600

    format and returning defaultRedirectUrl if no rules match

--- services/redirections/src/services/redirections-service.ts
+++ services/redirections/src/services/redirections-service.ts
@@ -1,37 +1,44 @@
-import {EvrythngClient} from "../clients/evrythng-client";
-import {ActionsClient} from "../clients/actions-client";
-import {RulesEngineClient} from "../clients/rules-engine-client";
+import { EvrythngClient } from "../clients/evrythng-client";
+import { ActionsClient } from "../clients/actions-client";
+import { RulesEngineClient } from "../clients/rules-engine-client";
 
 export interface RedirectionsService {
-    getRedirect(shortcode: string): Promise<string>
+  getRedirect(shortcode: string): Promise<string>;
 }
 
 export function RedirectionsService(
-    evrythngClient: EvrythngClient,
-    actionsClient: ActionsClient,
-    rulesEngineClient: RulesEngineClient): RedirectionsService {
-
-    async function getRedirect(shortcode: string): Promise<string> {
-        const redirect = await evrythngClient.getRedirection(shortcode);
-        if (!redirect) {
-            return ""
-        }
-
-        const meta = await evrythngClient.getRedirectMeta(redirect.accountId, redirect.thngId || redirect.productId)
-        if (!meta) {
-            return ""
-        }
-        const action = await actionsClient.create({});
-
-        const rules = await rulesEngineClient.evaluate(meta.rules, {
-            ...meta,
-            action
-        })
-
-        return rules.sort((a, b) => a.weight - b.weight)[0].redirectUrl;
+  evrythngClient: EvrythngClient,
+  actionsClient: ActionsClient,
+  rulesEngineClient: RulesEngineClient
+): RedirectionsService {
+  async function getRedirect(shortcode: string): Promise<string> {
+    const redirect = await evrythngClient.getRedirection(shortcode);
+    if (!redirect) {
+      return "";
     }
 
-    return {
-        getRedirect,
-    };
+    const meta = await evrythngClient.getRedirectMeta(
+      redirect.accountId,
+      redirect.thngId || redirect.productId
+    );
+    if (!meta) {
+      return "";
+    }
+    const action = await actionsClient.create({});
+
+    const rules = await rulesEngineClient.evaluate(meta.rules, {
+      ...meta,
+      action,
+    });
+
+    if (rules.length === 0) {
+      return redirect.defaultRedirectUrl;
+    }
+
+    return rules.sort((a, b) => a.weight - b.weight)[0].redirectUrl;
+  }
+
+  return {
+    getRedirect,
+  };
 }
