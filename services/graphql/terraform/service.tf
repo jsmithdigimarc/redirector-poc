fad4ffa4e61d79f585ea95065671f1be899aead5
Jake Smith <jsmith@digimarc.com>
2022-10-12 15:19:42 -0600

    For dev made graphql service accessible to authed accounts

--- services/graphql/terraform/service.tf
+++ services/graphql/terraform/service.tf
@@ -49,7 +49,8 @@ data "google_iam_policy" "private_service_policy" {
   binding {
     role    = "roles/run.invoker"
     members = [
-      "serviceAccount:${var.service_account_email}"
+      "serviceAccount:${var.service_account_email}",
+      "allAuthenticatedUsers"
     ]
   }
 }
