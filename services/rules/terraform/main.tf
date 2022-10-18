5686b303dbdb1d2da5a833dadce9f2a8a3ce6b23
Jake Smith <jsmith@digimarc.com>
2022-10-18 13:20:18 -0600

    Using digest instead of tag with cloud run

--- services/rules/terraform/main.tf
+++ services/rules/terraform/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.22.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "4.41.0"
+    }
+  }
+}
+
 locals {
   service_name = "rules-service"
 }
